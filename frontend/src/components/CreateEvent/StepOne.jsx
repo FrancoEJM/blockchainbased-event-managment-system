a371ace3f5293832fa07fd9b2a2a37e3d683c195
FrancoEJM <franco.mjimenez@gmail.com>
2024-05-05 11:29:59 -0400

    stepone

--- frontend/src/components/CreateEvent/StepOne.jsx
+++ frontend/src/components/CreateEvent/StepOne.jsx
@@ -1,8 +1,36 @@
+import { useState } from "react";
+
 function StepOne() {
+  const [invitados, setInvitados] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [modalidad, setModalidad] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleInputKeyPress = (event) => {
+    if (event.key === "Enter" && inputValue.trim() !== "") {
+      event.preventDefault(); // Evitar que el formulario se envíe al presionar Enter
+      setInvitados([...invitados, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  const handleRemoveInvitado = (index) => {
+    const nuevosInvitados = [...invitados];
+    nuevosInvitados.splice(index, 1);
+    setInvitados(nuevosInvitados);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Evitar que el formulario se envíe al presionar Enter
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="grid grid-cols-12 gap-4">
-        <div className="col-span-6 row-span-1">
+        <div className="col-span-6">
           <label
             className="block tracking-wide text-grey-darker text-xs font-bold mb-2"
             htmlFor="grid-first-name"
@@ -16,7 +44,7 @@ function StepOne() {
             placeholder="Ingrese el nombre del evento"
           />
         </div>
-        <div className="col-span-3 row-span-1">
+        <div className="col-span-3">
           <label
             className="block tracking-wide text-grey-darker text-xs font-bold mb-2"
             htmlFor="grid-first-name"
@@ -31,7 +59,7 @@ function StepOne() {
             placeholder="HH:mm"
           />
         </div>
-        <div className="col-span-3 row-span-1">
+        <div className="col-span-3">
           <label
             className="block tracking-wide text-grey-darker text-xs font-bold mb-2"
             htmlFor="grid-first-name"
@@ -46,7 +74,7 @@ function StepOne() {
             placeholder="HH:mm"
           />
         </div>
-        <div className="col-span-6">
+        <div className="col-span-6 row-span-2">
           <label
             className="block tracking-wide text-grey-darker text-xs font-bold mb-2"
             htmlFor="grid-first-name"
@@ -54,7 +82,7 @@ function StepOne() {
             Descripcion
           </label>
           <textarea
-            className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3 row-span-5"
+            className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3 h-36"
             id="grid-first-name"
             type="text"
             placeholder="Ingrese una descripción para su evento"
@@ -68,7 +96,7 @@ function StepOne() {
             Privacidad
           </label>
           <select
-            className=" block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3 row-span-5"
+            className=" block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3"
             id="grid-first-name"
             type="text"
             placeholder="Ingrese una descripción para su evento"
@@ -77,7 +105,6 @@ function StepOne() {
             <option value="2">2</option>
           </select>
         </div>
-
         <div className="col-span-2">
           <label
             className="block tracking-wide text-grey-darker text-xs font-bold mb-2"
@@ -86,7 +113,7 @@ function StepOne() {
             Idioma
           </label>
           <select
-            className=" block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3 row-span-5"
+            className=" block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3"
             id="grid-first-name"
             type="text"
             placeholder="Ingrese una descripción para su evento"
@@ -103,14 +130,78 @@ function StepOne() {
             Modalidad
           </label>
           <select
-            className=" block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3 row-span-5"
+            className=" block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3"
             id="grid-first-name"
             type="text"
             placeholder="Ingrese una descripción para su evento"
+            onChange={(e) => setModalidad(e.target.value)}
           >
             <option value="1">1</option>
             <option value="2">2</option>
           </select>
+        </div>
+        <div className="col-span-2">
+          <label
+            className="block tracking-wide text-grey-darker text-xs font-bold mb-2"
+            htmlFor="grid-first-name"
+          >
+            Lista de invitados
+          </label>
+          <input
+            className=" block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3"
+            id="grid-first-name"
+            type="text"
+            placeholder="Ingrese un invitado y presione Enter"
+            value={inputValue}
+            onChange={handleInputChange}
+            onKeyPress={handleInputKeyPress}
+          />
+        </div>
+        <div className="col-span-2">
+          <div className="overflow-y-auto h-16 mb-2">
+            {invitados.map((invitado, index) => (
+              <div
+                key={index}
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  borderBottom: "1px solid #ccc",
+                  padding: "5px",
+                }}
+              >
+                <span>{invitado}</span>
+                <button
+                  onClick={() => handleRemoveInvitado(index)}
+                  style={{
+                    background: "none",
+                    border: "none",
+                    cursor: "pointer",
+                  }}
+                >
+                  X
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="col-span-2">
+          {modalidad === "2" && (
+            <div>
+              <label
+                className="block tracking-wide text-grey-darker text-xs font-bold mb-2"
+                htmlFor="grid-first-name"
+              >
+                Url
+              </label>
+              <input
+                className=" block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3"
+                id="grid-first-name"
+                type="text"
+                placeholder="Ingrese la URL del evento"
+              />
+            </div>
+          )}
         </div>
       </div>
     </form>
